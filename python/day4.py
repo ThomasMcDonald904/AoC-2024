4370ac4516a7516725311c81d7d84c99a5209223
Thomas McDonald <4001137@keranna.qc.ca>
2024-12-05 23:18:31 -0500

    beginning of day 4

--- python/day4.py
+++ python/day4.py
@@ -1,16 +1,25 @@
 def day4(input: str):
     total = 0
-    total += find_horizontal(input) + find_vertical(input) + find_diagonal(input)
+    grid_input = [list(line) for line in input.splitlines()]
+    total += find_horizontal(input) + find_vertical(grid_input) + find_diagonal(grid_input)
     return total
 
 def find_horizontal(input: str) -> int:
-    return 0
+    return input.count("XMAS") + input.count("SAMX")
 
 
-def find_vertical(input: str) -> int:
-    return 0
 
-def find_diagonal(input: str) -> int:
+def find_vertical(grid_input: str) -> int:
+    count = 0
+    for row_index in range(len(grid_input) - 3):
+        for column_index in range(len(grid_input[0])):
+            if grid_input[row_index][column_index] == "X" and grid_input[row_index + 1][column_index] == "M" and grid_input[row_index + 2][column_index] == "A" and grid_input[row_index + 3][column_index] == "S":
+                count += 1
+            elif grid_input[row_index][column_index] == "S" and grid_input[row_index + 1][column_index] == "A" and grid_input[row_index + 2][column_index] == "M" and grid_input[row_index + 3][column_index] == "X":
+                count += 1
+    return count
+
+def find_diagonal(grid_input: str) -> int:
     return 0
 
 
